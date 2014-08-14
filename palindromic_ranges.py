7c85bc5ca46fd7e9ff93a0d18ce09d2dc0f6af9c
bryanjenningz <bryanjenningz@gmail.com>
2014-08-13 17:11:20 -0700

    Improved naming and structure

--- palindromic_ranges.py
+++ palindromic_ranges.py
@@ -12,18 +12,14 @@ import sys
 def is_pal(number):
     return str(number) == str(number)[::-1]
 
-def is_interesting(n_range):
-    if len(n_range) == 0: return False
-    number_pals = 0
-    for number in n_range:
-        if is_pal(number): number_pals += 1
-    return number_pals % 2 == 0
+def is_interesting(number_range):
+    return sum(1 for number in number_range if is_pal(number)) % 2 == 0
 
-def get_ranges(n_range):
-    ranges = [n_range]
-    for range_size in range(1, len(n_range)):
-        for i in range(len(n_range) - range_size + 1):
-            ranges.append(n_range[i:i + range_size])
+def get_ranges(number_range):
+    ranges = [number_range]
+    for range_size in range(1, len(number_range)):
+        for i in range(len(number_range) - range_size + 1):
+            ranges.append(number_range[i:i + range_size])
     return ranges
 
 with open(sys.argv[1], 'r') as file:
@@ -31,9 +27,8 @@ with open(sys.argv[1], 'r') as file:
 
 for line in lines:
     interestings = 0
-    L, R = line.strip().split(' ')
-    L, R = int(L), int(R)
-    for r in get_ranges(range(L, R+1)):
-        if is_interesting(r):
+    lower, upper = (int(i) for i in (line.strip().split(' ')))
+    for subset in get_ranges(range(lower, upper + 1)):
+        if is_interesting(subset):
             interestings += 1
     print(interestings)
